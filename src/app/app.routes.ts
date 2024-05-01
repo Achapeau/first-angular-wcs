2ddb6c083ac76aa8885b3d59e7732b955218e8b9
Achapeau <127884427+Achapeau@users.noreply.github.com>
2024-05-01 14:16:59 +0200

    up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { SignupComponent } from './menu/signup/signup.component';
+import { UserComponent } from './menu/user/user.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'signup', component: SignupComponent},
+    {path: 'user', component: UserComponent},
+    {path: '', component: UserProfileComponent},
+];
