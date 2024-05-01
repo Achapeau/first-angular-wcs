1a6f582847fecfeb3e778432eefd5157e4d59be9
Achapeau <127884427+Achapeau@users.noreply.github.com>
2024-05-01 13:48:07 +0200

    youhou!!!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
+import { NgIf, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgStyle, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = "Youhou!!!... (désolé, j'ai pas la passion)";
+  isAdmin: boolean = false;
+  title: string = "Youhou!!!... (désolé, j'ai pas la passion)";
+
+  onClick() {
+    this.isAdmin = !this.isAdmin;
+  }
 }
