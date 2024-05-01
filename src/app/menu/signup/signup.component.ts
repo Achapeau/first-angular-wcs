84b4d040239b5487a35ed927cb2fa2cc2458b00b
Achapeau <127884427+Achapeau@users.noreply.github.com>
2024-05-01 16:05:01 +0200

    form

--- src/app/menu/signup/signup.component.ts
+++ src/app/menu/signup/signup.component.ts
@@ -1,12 +1,46 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, NgIf],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
 
+  newOrder: Order = {
+    firstname: '',
+    lastname: '',
+    email: '',
+    password: ''
+  };
+
+  minDate: string;
+
+  constructor() {
+    const today = new Date();
+    const month = today.getMonth() + 1 < 10 ? '0' + (today.getMonth() + 1) : today.getMonth() + 1;
+    const day = today.getDate() < 10 ? '0' + today.getDate() : today.getDate();
+    this.minDate = today.getFullYear() + '-' + month + '-' + day;
+   }
+
+
+  
+  
+  onSubmit() {
+    console.table(this.newOrder);
+    confirm("Confirmer vous les données suivantes?" + JSON.stringify(this.newOrder));
+
+  }
+  
+}
+
+export interface Order {
+  firstname: string;
+  lastname: string;
+  email: string;
+  password: string;
 }
