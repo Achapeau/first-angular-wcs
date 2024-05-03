8bf4d0a7c5a1f1862a61a4f4a623f155133d2995
Achapeau <127884427+Achapeau@users.noreply.github.com>
2024-05-03 16:17:42 +0200

    update

--- src/app/core/log.interceptor.ts
+++ src/app/core/log.interceptor.ts
@@ -1,18 +1,17 @@
-import { HttpEvent, HttpHandler, HttpInterceptorFn, HttpRequest, HttpResponse } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Observable, tap } from "rxjs";
+import { HttpInterceptorFn, HttpResponse } from "@angular/common/http";
+import { tap } from "rxjs";
 
 export const logInterceptor: HttpInterceptorFn = (req, next) => {
     var start = new Date().getTime();
     var requete = req.url
 
     return next(req).pipe(
-    tap((res) => {
-        if (res instanceof HttpResponse) {
-            var elapsed = new Date().getTime() - start;
-            console.log(`delay for the response from ${requete}: ${elapsed} ms`);            
-        }
-    })
+        tap((res) => {
+            if (res instanceof HttpResponse) {
+                var elapsed = new Date().getTime() - start;
+                console.log(`delay for the response from ${requete}: ${elapsed} ms`);            
+            }
+        })
     )
 
 }
